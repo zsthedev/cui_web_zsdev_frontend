51ddf16ea019a9e0302682d58d9d352ce88edc4d
Shahzaib Khan <zsdev166@gmail.com>
2024-05-18 11:53:42 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <section className="w-full h-screen bg-green-500"></section>;
+};
+
+export default Home;
